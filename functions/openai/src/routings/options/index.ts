af76fba618d7a900710f71aa38a533c87a6c36df
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-05-16 18:59:17 +0200

    update

--- functions/openai/src/routings/options/index.ts
+++ functions/openai/src/routings/options/index.ts
@@ -6,8 +6,8 @@ export * from './pubsubOptions'
 export * from './firestoreOptions'
 export * from './schedulerOptions'
 
-export const project = process.env.PROJECT_ID || 'figaro-labs'
+export const project = process.env.PROJECT_ID || 'skeet-example'
 export const region = process.env.REGION || 'europe-west6'
 export const serviceAccount = `${project}@${project}.iam.gserviceaccount.com`
 export const vpcConnector = `${project}-con`
-export const cors = ['http://localhost:4000', 'https://app.figaro.one']
+export const cors = ['http://localhost:4000', 'https://app.skeet.dev']
