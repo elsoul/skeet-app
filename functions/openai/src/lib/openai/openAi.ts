ab4d6e70e8308d22b699fe39fbe3c6b9cf7ec520
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-05-22 13:45:19 +0200

    add openai stream method

--- functions/openai/src/lib/openai/openAi.ts
+++ functions/openai/src/lib/openai/openAi.ts
@@ -1,5 +1,11 @@
 import dotenv from 'dotenv'
 import { Configuration, CreateChatCompletionRequest, OpenAIApi } from 'openai'
+import { IncomingMessage } from 'http'
+import { User, UserChatRoom, UserChatRoomMessage } from '@/models'
+import {
+  getGrandChildCollectionItem,
+  updateGrandChildCollectionItem,
+} from '@skeet-framework/firestore'
 dotenv.config()
 
 export const chat = async (
@@ -14,4 +20,88 @@ export const chat = async (
     createChatCompletionRequest
   )
   return completion.data.choices[0].message
+}
+
+export const streamChat = async (
+  userId: string,
+  userChatRoomId: string,
+  userChatRoomMessageId: string,
+  createChatCompletionRequest: CreateChatCompletionRequest
+) => {
+  const configuration = new Configuration({
+    organization: process.env.CHAT_GPT_ORG,
+    apiKey: process.env.CHAT_GPT_KEY,
+  })
+  const openai = new OpenAIApi(configuration)
+  try {
+    const res = await openai.createChatCompletion(createChatCompletionRequest, {
+      responseType: 'stream',
+    })
+    const stream = res.data as unknown as IncomingMessage
+    stream.on('data', async (chunk: Buffer) => {
+      const payloads = chunk.toString().split('\n\n')
+      for await (const payload of payloads) {
+        if (payload.includes('[DONE]')) return
+        if (payload.startsWith('data:')) {
+          const data = payload.replaceAll(/(\n)?^data:\s*/g, '')
+          try {
+            const delta = JSON.parse(data.trim())
+            const message = delta.choices[0].delta?.content
+            if (message == undefined) continue
+            const collectionName = 'User'
+            const childCollectionName = 'UserChatRoom'
+            const grandChildCollectionName = 'UserChatRoomMessage'
+            const userChatRoomMessage = await getGrandChildCollectionItem<
+              UserChatRoomMessage,
+              UserChatRoom,
+              User
+            >(
+              collectionName,
+              childCollectionName,
+              grandChildCollectionName,
+              userId,
+              userChatRoomId,
+              userChatRoomMessageId
+            )
+            const currentContent = userChatRoomMessage.data.content
+            const newContent = currentContent + message
+            await updateGrandChildCollectionItem<
+              UserChatRoomMessage,
+              UserChatRoom,
+              User
+            >(
+              collectionName,
+              childCollectionName,
+              grandChildCollectionName,
+              userId,
+              userChatRoomId,
+              userChatRoomMessageId,
+              { content: newContent }
+            )
+            console.log(message)
+          } catch (error) {
+            console.log(`Error with JSON.parse and ${payload}.\n${error}`)
+          }
+        }
+      }
+    })
+
+    stream.on('end', () => console.log('Stream done'))
+    stream.on('error', (e: Error) => console.error(e))
+  } catch (error: any) {
+    if (error.response?.status) {
+      console.error(error.response.status, error.message)
+      error.response.data.on('data', (data: any) => {
+        const message = data.toString()
+        try {
+          const parsed = JSON.parse(message)
+          console.error('An error occurred during OpenAI request: ', parsed)
+        } catch (error) {
+          console.error('An error occurred during OpenAI request: ', message)
+        }
+      })
+    } else {
+      console.error('An error occurred during OpenAI request', error)
+    }
+  }
 }
