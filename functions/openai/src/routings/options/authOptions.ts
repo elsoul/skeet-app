91b92573437a70ffd1cfbf95ea91477988917ecf
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-05-26 12:44:17 +0200

    update service account

---
+++ functions/openai/src/routings/options/authOptions.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+import { RuntimeOptions } from 'firebase-functions/v1'
+dotenv.config()
+
+const appName = process.env.SKEET_APP_NAME || 'skeet-app'
+const project = process.env.PROJECT_ID || 'skeet-app'
+const serviceAccount = `${appName}@${project}.iam.gserviceaccount.com`
+const vpcConnector = `${project}-con`
+
+export const authDefaultOption: RuntimeOptions = {
+  memory: '1GB',
+  maxInstances: 100,
+  minInstances: 0,
+  timeoutSeconds: 300,
+  serviceAccount,
+  ingressSettings: 'ALLOW_INTERNAL_ONLY',
+  vpcConnector,
+  vpcConnectorEgressSettings: 'PRIVATE_RANGES_ONLY',
+}
