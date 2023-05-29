118c9c5160a12949dc5c63e04a188b367382db3d
KishiTheMechanic <s.kishi@elsoul.nl>
2023-05-29 22:19:15 +0200

    fix useSkeetFunctions

--- src/hooks/useSkeetFunctions.ts
+++ src/hooks/useSkeetFunctions.ts
@@ -1,18 +1,24 @@
-import { userState } from "@/store/user"
+import { userState } from '@/store/user'
 import { useRecoilValue } from 'recoil'
 import skeetCloudConfig from '@root/skeet-cloud.config.json'
+import { toKebabCase } from '@/utils/character'
 
-export default function useSkeetFunctions(methodName: string) {
-  const {skeetToken} = useRecoilValue(userState)
-  const fetcher = async () => {
+export default function useSkeetFunctions() {
+  const { skeetToken } = useRecoilValue(userState)
+  const fetcher = async (functionName: string, methodName: string) => {
     try {
-      const url = process.env.NODE_ENV === 'production' ? `https://${skeetCloudConfig.app.functionsDomain}` : `http://127.0.0.1:5001/${skeetCloudConfig.app.projectId}/${skeetCloudConfig.app.region}`
-      const res = await fetch(`${url}/${methodName}`, {
+      const url =
+        process.env.NODE_ENV === 'production'
+          ? `https://${
+              skeetCloudConfig.app.functionsDomain
+            }/${functionName}/${toKebabCase(methodName)}`
+          : `http://127.0.0.1:5001/${skeetCloudConfig.app.projectId}/${skeetCloudConfig.app.region}/${methodName}`
+      const res = await fetch(`${url}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${skeetToken}`
-        }
+          Authorization: `Bearer ${skeetToken}`,
+        },
       })
       return await res.json()
     } catch (e) {
