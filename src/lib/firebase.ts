439106e35c85875285f53eaf8d49e9f98a744226
KishiTheMechanic <s.kishi@elsoul.nl>
2023-08-09 02:11:02 +0200

    fix the dev environment with firebase emulator

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,27 +3,27 @@ import { getAnalytics } from 'firebase/analytics'
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { connectAuthEmulator, getAuth } from 'firebase/auth'
 import { getStorage, connectStorageEmulator } from 'firebase/storage'
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+import {
+  connectFirestoreEmulator,
+  initializeFirestore,
+} from 'firebase/firestore'
 import { Platform } from 'react-native'
 
 export const firebaseApp = !getApps().length
   ? initializeApp(firebaseConfig)
   : getApp()
 
+export const platformDevIP =
+  Platform.OS === 'web'
+    ? '127.0.0.1'
+    : Platform.OS === 'android'
+    ? '10.0.2.2'
+    : '0.0.0.0'
+
 const getFirebaseAuth = () => {
   const firebaseAuth = getAuth(firebaseApp)
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'web') {
-    connectAuthEmulator(firebaseAuth, 'http://127.0.0.1:9099', {
-      disableWarnings: true,
-    })
-  }
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'android') {
-    connectAuthEmulator(firebaseAuth, 'http://10.0.2.2:9099', {
-      disableWarnings: true,
-    })
-  }
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'ios') {
-    connectAuthEmulator(firebaseAuth, 'http://0.0.0.0:9099', {
+  if (process.env.NODE_ENV !== 'production') {
+    connectAuthEmulator(firebaseAuth, `http://${platformDevIP}:9099`, {
       disableWarnings: true,
     })
   }
@@ -34,30 +34,21 @@ export const auth = firebaseApp ? getFirebaseAuth() : undefined
 
 const getFirebaseStorage = () => {
   const firebaseStorage = getStorage(firebaseApp)
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'web') {
-    connectStorageEmulator(firebaseStorage, '127.0.0.1', 9199)
+  if (process.env.NODE_ENV !== 'production') {
+    connectStorageEmulator(firebaseStorage, platformDevIP, 9199)
   }
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'android') {
-    connectStorageEmulator(firebaseStorage, '10.0.2.2', 9199)
-  }
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'ios') {
-    connectStorageEmulator(firebaseStorage, '0.0.0.0', 9199)
-  }
+
   return firebaseStorage
 }
 
 export const storage = firebaseApp ? getFirebaseStorage() : undefined
 
 const getFirebaseFirestore = () => {
-  const firestoreDb = getFirestore(firebaseApp)
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'web') {
-    connectFirestoreEmulator(firestoreDb, '127.0.0.1', 8080)
-  }
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'android') {
-    connectFirestoreEmulator(firestoreDb, '10.0.2.2', 8080)
-  }
-  if (process.env.NODE_ENV !== 'production' && Platform.OS === 'ios') {
-    connectFirestoreEmulator(firestoreDb, '0.0.0.0', 8080)
+  const firestoreDb = initializeFirestore(firebaseApp, {
+    experimentalForceLongPolling: true,
+  })
+  if (process.env.NODE_ENV !== 'production') {
+    connectFirestoreEmulator(firestoreDb, platformDevIP, 8080)
   }
   return firestoreDb
 }
