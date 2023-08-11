43c8f4924bf5a1d9ce293ed50e88334b2af46c4f
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-08-11 20:08:55 +0200

    add login

---
+++ functions/skeet/src/lib/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAxTADDsXI9QENVObjIb7HVAj5LMMy3L0o',
+  authDomain: 'skeet-graphql.firebaseapp.com',
+  projectId: 'skeet-graphql',
+  storageBucket: 'skeet-graphql.appspot.com',
+  messagingSenderId: '818638014982',
+  appId: '1:818638014982:web:2b634dc809a351340a9187',
+  measurementId: 'G-VSD8SQ0FZD',
+}
+
+export default firebaseConfig
