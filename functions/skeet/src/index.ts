e6e4444e68b7e87558dc2ccd032ce6c763dc54db
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-08-29 20:26:08 +0200

    rm some files

--- functions/skeet/src/index.ts
+++ functions/skeet/src/index.ts
@@ -11,5 +11,4 @@ export {
   addVertexMessage,
   createUserChatRoom,
   addUserChatRoomMessage,
-  generateTitle,
 } from '@/routings'
--- functions/skeet/src/routings/pubsub/index.ts
+++ functions/skeet/src/routings/pubsub/index.ts
@@ -1,2 +1,1 @@
 export * from './pubsubExample'
-export * from './generateTitle'
