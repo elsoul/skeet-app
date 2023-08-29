c2d9cb3dddcdb2bec406dfcde4c323574a639097
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-08-29 19:55:21 +0200

    update package

--- functions/skeet/src/index.ts
+++ functions/skeet/src/index.ts
@@ -11,4 +11,5 @@ export {
   addVertexMessage,
   createUserChatRoom,
   addUserChatRoomMessage,
+  generateTitle,
 } from '@/routings'
--- functions/skeet/src/routings/pubsub/index.ts
+++ functions/skeet/src/routings/pubsub/index.ts
@@ -1,1 +1,2 @@
 export * from './pubsubExample'
+export * from './generateTitle'
