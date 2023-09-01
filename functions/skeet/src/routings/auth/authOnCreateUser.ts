63070f9593d1749588d02ccf187896652b273dc6
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-09-01 14:28:03 +0200

    fix import for authOncreate routings/options

--- functions/skeet/src/routings/auth/authOnCreateUser.ts
+++ functions/skeet/src/routings/auth/authOnCreateUser.ts
@@ -2,7 +2,7 @@ import { db } from '@/index'
 import { User } from '@/models'
 import { add } from '@skeet-framework/firestore'
 import * as functions from 'firebase-functions/v1'
-import { authPublicOption } from '@/routings'
+import { authPublicOption } from '@/routings/options'
 import { gravatarIconUrl } from '@skeet-framework/utils'
 import skeetConfig from '../../../skeetOptions.json'
 const region = skeetConfig.region
